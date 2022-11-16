04a6d02e8a5ba6f4e191688b5f1d486f260706c1
guilhermemap <86965518+guilhermemap@users.noreply.github.com>
2022-11-15 23:12:09 -0300

    finalizando config

--- index.js
+++ index.js
@@ -7,6 +7,41 @@ const conn = require('./db/conn');
 const port = 3000;
 const app = express();
 
+app.engine = ('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(express.json());
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new Filestore({
+            logFn: function () { },
+            path: require('path').join(require('os').tmpdir(), 'sessions')
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true //pra localhost, precisaria de mais config pra https
+        }
+    })
+);
+app.use(flash());
+app.use(express.static('public'));
+
+app.use((req, res, next) => {
+    if (req.session.userid) {
+        res.locals.sessions = req.session;
+    }
+    next();
+});
 
 conn
     .sync()
