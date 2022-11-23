d569bb5860b6bd53b97f508d2d873110a51c05a3
guilhermemap <86965518+guilhermemap@users.noreply.github.com>
2022-11-22 22:19:24 -0300

    confere senhas e faz mensagem de erro

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,20 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render("auth/login");
     }
     static register(req, res) {
         res.render("auth/register");
+    }
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body;
+        if (password != confirmpassword) {
+            //mensagem de senhas diferentes
+            req.flash('message', 'As senhas não conferem!');
+            res.render('auth/register');
+            return;
+        }
     }
 };
