1027121d54bd6b331b1f9d3f136cf279ac293927
guilhermemap <86965518+guilhermemap@users.noreply.github.com>
2022-11-22 21:10:59 -0300

    css inicial

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -3,7 +3,6 @@ const User = require('../models/User');
 
 module.exports = class ThoughtController {
     static async showAll(req, res) {
-        console.log('showing all...');
         res.render('thoughts/home');
     }
 };
